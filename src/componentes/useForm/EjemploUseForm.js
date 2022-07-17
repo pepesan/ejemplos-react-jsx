23b7e244c9b11c28dcb2fb3ef9cfdfba5157216c
David Vaquero <pepesan@gmail.com>
2022-07-17 19:56:22 +0200

    mejorado ejemplo con useForm de react-hook-form

--- src/componentes/useForm/EjemploUseForm.js
+++ src/componentes/useForm/EjemploUseForm.js
@@ -7,19 +7,19 @@ function EjemploUserForm() {
         <div>
             <h2>Ejemplo React hook Form</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-3">
+                <div className="mb-3 form-group">
                     <label htmlFor="example" className="form-label">Ejemplo</label>
                     {/* register your input into the hook by invoking the "register" function */}
                     <input id="example" className="form-control" defaultValue="test" {...register("example")} />
                 </div>
-                <div className="mb-3">
+                <div className="mb-3 form-group">
                     <label htmlFor="exampleRequired" className="form-label">Example Required</label>
                     {/* include validation with required or other standard HTML validation rules */}
                     <input id="exampleRequired" className="form-control" {...register("exampleRequired", { required: true })} />
                     {/* errors will return when field validation fails  */}
                     {errors.exampleRequired && <span>This field is required</span>}
                 </div>
-                <div className="mb-3">
+                <div className="mb-3 form-group">
                     <input type="submit" />
                 </div>
             </form>
